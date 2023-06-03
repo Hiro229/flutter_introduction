2b632715723f93b5d869a6b15dc9086e670c312c
TJ <zigzag37458@gmail.com>
2023-06-03 09:20:07 +0900

    [add]記事ページ

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'grid_view_page.dart';
+import 'sliver_app_bar_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: GridViewPage(),
+      home: SliverAppBarPage(),
     );
   }
 }
