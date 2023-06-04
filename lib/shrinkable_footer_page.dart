cc241e546783450e23a64edb7da798deeeda1d79
TJ <zigzag37458@gmail.com>
2023-06-04 11:43:52 +0900

    [fix]フッターのはみ出し、アイコンとテキストの重なりを修正

--- lib/shrinkable_footer_page.dart
+++ lib/shrinkable_footer_page.dart
@@ -138,21 +138,25 @@ class _BottomNavigationBar extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             _IconTextItem(
+              isHiding: isHiding,
               deviceWidth: width,
               icon: Icons.home,
               title: 'Home',
             ),
             _IconTextItem(
+              isHiding: isHiding,
               deviceWidth: width,
               icon: Icons.star,
               title: 'Favorite',
             ),
             _IconTextItem(
+              isHiding: isHiding,
               deviceWidth: width,
               icon: Icons.favorite,
               title: 'Like',
             ),
             _IconTextItem(
+              isHiding: isHiding,
               deviceWidth: width,
               icon: Icons.settings,
               title: 'Menu',
@@ -168,29 +172,42 @@ class _IconTextItem extends StatelessWidget {
   const _IconTextItem({
     Key? key,
     required this.deviceWidth,
+    required this.isHiding,
     required this.icon,
     required this.title,
   }) : super(key: key);
 
   final double deviceWidth;
+  final bool isHiding;
   final IconData icon;
   final String title;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
+      height: 48,
       width: 0.25 * (deviceWidth - 32),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      child: Stack(
+        // mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Icon(
-            icon,
-            color: Color(0xFF442C2E),
-            size: 24,
+          Align(
+            alignment: Alignment.topCenter,
+            child: Icon(
+              icon,
+              color: Color(0xFF442C2E),
+              size: 24,
+            ),
           ),
-          Text(
-            title,
-            style: TextStyle(fontSize: 16),
+          AnimatedOpacity(
+            opacity: isHiding ? 0 : 1,
+            duration: Duration(milliseconds: 120),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                title,
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
           ),
         ],
       ),
