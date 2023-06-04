627ea7dd805577f753e1a1532578d6f77e57c7df
TJ <zigzag37458@gmail.com>
2023-06-04 10:42:08 +0900

    [add]フッターを使用したページの追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'grid_view_page.dart';
 import 'sliver_app_bar_page.dart';
+import 'shrinkable_footer_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SliverAppBarPage(),
+      home: ShrinkableFooterPage(),
     );
   }
 }
---
+++ lib/shrinkable_footer_page.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class ShrinkableFooterPage extends StatefulWidget {
+  const ShrinkableFooterPage({Key? key}) : super(key: key);
+
+  @override
+  _ShrinkableFooterPageState createState() => _ShrinkableFooterPageState();
+}
+
+class _ShrinkableFooterPageState extends State<ShrinkableFooterPage> {
+  final _scrollController = ScrollController();
+  bool isHiding = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _listenScroll();
+  }
+
+  // ウィジェット利用終了時に呼び出されるdisposeにコントローラーのスクロール検知を解除する。
+  @override
+  void dispose() {
+    _scrollController.removeListener(() {});
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: _BottomNavigationBar(isHiding: isHiding),
+      body: SafeArea(
+        child: ListView(
+          controller: _scrollController,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 16, right: 50),
+              height: 200,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(20.0),
+                  bottomLeft: Radius.circular(20.0),
+                ),
+                color: Color(0xFFFEEAE6),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const SizedBox(
+                    height: 70,
+                  ),
+                  Text(
+                    'スクロールに応じて\nBottomNavigationBarが\n知事見ます。',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24.0,
+                        color: Color(0xFF442C2E),
+                        height: 1.2),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            Image.asset(
+              'images/flower-field.jpg',
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 20.0),
+            Image.asset(
+              'images/learning.jpg',
+            ),
+            const SizedBox(height: 20.0),
+            Image.asset(
+              'forest.jpg',
+            ),
+            const SizedBox(height: 20.0),
+            Image.asset(
+              'images/road.jpg',
+              fit: BoxFit.cover,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _listenScroll() {
+    _scrollController.addListener(() {
+      if (_scrollController.position.userScrollDirection ==
+          ScrollDirection.reverse) {
+        isHiding = true;
+      } else {
+        isHiding = false;
+      }
+      setState(() {});
+    });
+  }
+}
+
+class _BottomNavigationBar extends StatelessWidget {
+  _BottomNavigationBar({required this.isHiding});
+
+  final bool isHiding;
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return Container(
+      height: 60,
+      color: Color(0xFFFEEAE6),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _IconTextItem(
+              deviceWidth: width,
+              icon: Icons.home,
+              title: 'Home',
+            ),
+            _IconTextItem(
+              deviceWidth: width,
+              icon: Icons.star,
+              title: 'Favorite',
+            ),
+            _IconTextItem(
+              deviceWidth: width,
+              icon: Icons.favorite,
+              title: 'Like',
+            ),
+            _IconTextItem(
+              deviceWidth: width,
+              icon: Icons.settings,
+              title: 'Menu',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _IconTextItem extends StatelessWidget {
+  const _IconTextItem({
+    Key? key,
+    required this.deviceWidth,
+    required this.icon,
+    required this.title,
+  }) : super(key: key);
+
+  final double deviceWidth;
+  final IconData icon;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 0.25 * (deviceWidth - 32),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            color: Color(0xFF442C2E),
+            size: 24,
+          ),
+          Text(
+            title,
+            style: TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
+    );
+  }
+}
